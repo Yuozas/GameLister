84ac5fac2654a8dca7d3816b28033fb34007315f
Yuozas <53146589+Yuozas@users.noreply.github.com>
2022-05-07 04:12:17 +0300

    Additional program reader behaviour.

--- GameLister/Services/ProgramWriters/ConsoleReader.cs
+++ GameLister/Services/ProgramWriters/ConsoleReader.cs
@@ -15,9 +15,37 @@ namespace GameLister.Services.ProgramWriters
             {
                 var line = ReadLine();
                 if(line is null)
-                    writer.WriteLine("No empty lines allowed");
+                    writer.WriteLine("No empty lines allowed.");
                 else
                     return line;
+            }
+        }
+
+        public string ReadLineDeclineIncorrect(IProgramWriter writer, params string[] options)
+        {
+            while (true)
+            {
+                var line = ReadLine();
+                if (line is null)
+                    writer.WriteLine("No empty lines allowed.");
+                else if (!options.Contains(line))
+                    writer.WriteLine("No such option.");
+                else
+                    return line;
+            }
+        }
+
+        public long ReadNumberLineDeclineEmpty(IProgramWriter writer)
+        {
+            while (true)
+            {
+                var line = ReadLine();
+                if (line is null)
+                    writer.WriteLine("No empty lines allowed.");
+                else if(!long.TryParse(line, out var number))
+                    writer.WriteLine("Only numbers allowed.");
+                else
+                    return number;
             }
         }
     }
