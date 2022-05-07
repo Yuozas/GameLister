2535d1265afd8fba532770c34ca6e875339adfac
Yuozas <53146589+Yuozas@users.noreply.github.com>
2022-05-07 20:21:12 +0300

    Unicode writer and reader.

--- GameLister/Services/ProgramWriters/ConsoleWritter.cs
+++ GameLister/Services/ProgramWriters/ConsoleWritter.cs
@@ -1,9 +1,15 @@
 ﻿using GameLister.Services.ProgramWriters.Templates;
+using System.Text;
 
 namespace GameLister.Services.ProgramWriters;
 
 public class ConsoleWriter : IProgramWriter
 {
+    public ConsoleWriter(Encoding encoding)
+    {
+        Console.InputEncoding = encoding;
+    }
+
     public void Clear()
     {
         Console.Clear();
